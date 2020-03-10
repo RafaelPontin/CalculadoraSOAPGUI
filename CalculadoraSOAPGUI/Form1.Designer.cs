36b11a4906f9be349faa98d80b1c329426fd7127
rafael pontin <rfmpontin@gmail.com>
2020-03-10 20:19:17 -0300

    Adicionar arquivos de projeto.

---
+++ CalculadoraSOAPGUI/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace CalculadoraSOAPGUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNum1 = new System.Windows.Forms.TextBox();
+            this.txtNum2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btSomar = new System.Windows.Forms.Button();
+            this.btDiv = new System.Windows.Forms.Button();
+            this.btMulti = new System.Windows.Forms.Button();
+            this.BtSub = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtTotal = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtNum1
+            // 
+            this.txtNum1.Location = new System.Drawing.Point(115, 51);
+            this.txtNum1.Name = "txtNum1";
+            this.txtNum1.Size = new System.Drawing.Size(174, 20);
+            this.txtNum1.TabIndex = 0;
+            // 
+            // txtNum2
+            // 
+            this.txtNum2.Location = new System.Drawing.Point(115, 86);
+            this.txtNum2.Name = "txtNum2";
+            this.txtNum2.Size = new System.Drawing.Size(174, 20);
+            this.txtNum2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(127, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(162, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Calculado SOAP";
+            // 
+            // btSomar
+            // 
+            this.btSomar.Location = new System.Drawing.Point(338, 28);
+            this.btSomar.Name = "btSomar";
+            this.btSomar.Size = new System.Drawing.Size(75, 23);
+            this.btSomar.TabIndex = 3;
+            this.btSomar.Text = "+";
+            this.btSomar.UseVisualStyleBackColor = true;
+            this.btSomar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btDiv
+            // 
+            this.btDiv.Location = new System.Drawing.Point(338, 115);
+            this.btDiv.Name = "btDiv";
+            this.btDiv.Size = new System.Drawing.Size(75, 23);
+            this.btDiv.TabIndex = 4;
+            this.btDiv.Text = "/";
+            this.btDiv.UseVisualStyleBackColor = true;
+            this.btDiv.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btMulti
+            // 
+            this.btMulti.Location = new System.Drawing.Point(338, 86);
+            this.btMulti.Name = "btMulti";
+            this.btMulti.Size = new System.Drawing.Size(75, 23);
+            this.btMulti.TabIndex = 5;
+            this.btMulti.Text = "*";
+            this.btMulti.UseVisualStyleBackColor = true;
+            this.btMulti.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // BtSub
+            // 
+            this.BtSub.Location = new System.Drawing.Point(338, 57);
+            this.BtSub.Name = "BtSub";
+            this.BtSub.Size = new System.Drawing.Size(75, 23);
+            this.BtSub.TabIndex = 6;
+            this.BtSub.Text = "-";
+            this.BtSub.UseVisualStyleBackColor = true;
+            this.BtSub.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Num1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(26, 92);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Num2";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(27, 127);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(31, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Total";
+            // 
+            // txtTotal
+            // 
+            this.txtTotal.Location = new System.Drawing.Point(115, 120);
+            this.txtTotal.Name = "txtTotal";
+            this.txtTotal.Size = new System.Drawing.Size(174, 20);
+            this.txtTotal.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(459, 178);
+            this.Controls.Add(this.txtTotal);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.BtSub);
+            this.Controls.Add(this.btMulti);
+            this.Controls.Add(this.btDiv);
+            this.Controls.Add(this.btSomar);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtNum2);
+            this.Controls.Add(this.txtNum1);
+            this.Name = "Form1";
+            this.Text = "Calculadora SOAP";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNum1;
+        private System.Windows.Forms.TextBox txtNum2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btSomar;
+        private System.Windows.Forms.Button btDiv;
+        private System.Windows.Forms.Button btMulti;
+        private System.Windows.Forms.Button BtSub;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtTotal;
+    }
+}
+
---
+++ CalculadoraSOAPGUI/Form1.cs
@@ -0,0 +1,49 @@
+﻿using CalculadoraSOAPGUI.ServiceReference;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculadoraSOAPGUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            CalculoServiceClient calculoServiceClient = new CalculoServiceClient();
+            decimal total = calculoServiceClient.Dividir(Convert.ToDecimal(txtNum1.Text), Convert.ToDecimal(txtNum2.Text));
+            txtTotal.Text = total.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            CalculoServiceClient calculoServiceClient = new CalculoServiceClient();
+            decimal total = calculoServiceClient.Somar(Convert.ToDecimal(txtNum1.Text), Convert.ToDecimal(txtNum2.Text));
+            txtTotal.Text = total.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            CalculoServiceClient calculoServiceClient = new CalculoServiceClient();
+            decimal total = calculoServiceClient.Multiplicar(Convert.ToDecimal(txtNum1.Text), Convert.ToDecimal(txtNum2.Text));
+            txtTotal.Text = total.ToString();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            CalculoServiceClient calculoServiceClient = new CalculoServiceClient();
+            decimal total = calculoServiceClient.Subtrair(Convert.ToDecimal(txtNum1.Text), Convert.ToDecimal(txtNum2.Text));
+            txtTotal.Text = total.ToString();
+        }
+    }
+}
